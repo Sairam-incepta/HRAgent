09ca08b61dfef21ff984bf48cc96d8716340294f
Adhish Chakravorty <skjetly094@gmail.com>
2025-06-13 14:50:19 -0700

    final final

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true 
+  },
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'out',
 };
 
 module.exports = nextConfig;
